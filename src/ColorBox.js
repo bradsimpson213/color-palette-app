e6e8030db201e7fd463f9ce2c2dace289d3cf4fd
bradsimpson213 <bsimpson@appacademy.io>
2020-12-29 23:17:51 -0500

    color boxes mostly working

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -1,18 +1,38 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { CopyToClipboard } from "react-copy-to-clipboard";
+import useToggleState from "./hooks/useToggleState";
 import "./ColorBox.css"
 
 const ColorBox = (props) => {
     const { name, background } = props;
+    const [ copy, toggleCopy ] = useToggleState(false)
+
+    const changeCopyState = () => {
+        toggleCopy(); 
+        setTimeout(() => toggleCopy, 1500);
+        // refactor at some point for callback
+    };
+
     return (
-        <div style={{ background }} className="ColorBox">
-            <div className="copy-container">
-                <div className="box-content">
-                    <span>{ name }</span>
+        <CopyToClipboard text={ background } onCopy={ changeCopyState }>
+            <div style={{ background }} className="ColorBox">
+                <div 
+                    style={{ background }} 
+                    className={ `copy-overlay ${copy && "show"}` } 
+                />
+                <div className={ `copy-message ${copy && "show"}` }>
+                    <h1>copied!</h1>
+                    <p>{ background }</p>
                 </div>
-                <button className="copy-button">Copy</button>
+                <div className="copy-container">
+                    <div className="box-content">
+                        <span>{ name }</span>
+                    </div>
+                    <button className="copy-button">Copy</button>
+                </div>
+                <span className="see-more">More</span>
             </div>
-            <span className="see-more">More</span>
-        </div>
+        </CopyToClipboard>
     )
 };
 
