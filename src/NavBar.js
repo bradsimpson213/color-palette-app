b73d2a3bf8c365a2cb2a06183c72931f95172e9c
bradsimpson213 <bsimpson@appacademy.io>
2021-01-01 22:58:29 -0500

    added snackbar with functionality

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,7 +1,11 @@
 import React, { useState } from "react";
+import useToggleState from "./hooks/useToggleState";
 import Slider from "rc-slider";
 import Select from "@material-ui/core/Select";
 import MenuItem from "@material-ui/core/MenuItem";
+import Snackbar from "@material-ui/core/Snackbar";
+import IconButton from "@material-ui/core/IconButton";
+import CloseIcon from "@material-ui/icons/Close";
 import "rc-slider/assets/index.css";
 import "./NavBar.css";
 
@@ -9,10 +13,12 @@ import "./NavBar.css";
 const Navbar = (props) => {
     const { level, setLevel } = props;
     const [format, setFormat] = useState("hex");
+    const [open, toggleOpen] = useToggleState(false);
 
     const changeFormat = (e) => {
         setFormat(e.target.value );
         props.handleChange(e.target.value);
+        toggleOpen()
     };
 
     return (
@@ -39,6 +45,24 @@ const Navbar = (props) => {
                     <MenuItem value="rgba">RGBA - rgba(255,255,255,1.0)</MenuItem>
                 </Select>
             </div>
+            <Snackbar 
+                anchorOrigin={{ vertical: "bottom", horizontal: "left" }}
+                open={ open }
+                autoHideDuration={3000}
+                message={ <span id="message-id">Format Changed to { format.toUpperCase() }!</span>}
+                ContentProps={{ "aria-describedby": "message-id"}}
+                onClose={ toggleOpen }
+                action={ [
+                    <IconButton 
+                        onClick= { toggleOpen }
+                        color="inherit"
+                        key="close"
+                        aria-label="close-snackbar"
+                    >
+                        <CloseIcon />
+                    </IconButton>
+                ]}
+            />
         </header>
     )
 }
