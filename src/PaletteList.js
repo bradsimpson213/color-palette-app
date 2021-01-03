31a05de1bb14dc6518de3fc7444af275abd14666
bradsimpson213 <bsimpson@appacademy.io>
2021-01-02 23:18:00 -0500

    syling palette list

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -1,23 +1,58 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import MiniPalette from "./MiniPalette";
+import { withStyles } from "@material-ui/styles";
+
+const styles = {
+    root: {
+        backgroundColor: "blue",
+        height: "100%",
+        display: "flex",
+        alignItems: "flex-start",
+        justifyContent: "center"
+    },
+    container: {
+        width: "50%",
+        display: "flex",
+        alignItems: "flex-start",
+        flexDirection: "column",
+        flexWrap: "wrap",
+    }, 
+    nav: {
+        display: "flex",
+        width: "100%",
+        justifyContent: "space-between",
+        color: "white"
+    },
+    palettes: {
+        boxSizing: "border-box",
+        width: "100%",
+        display: "grid",
+        gridTemplateColumns: "repeat(3, 30%)",
+        gridGap: "5%",
+
+    }
+};
 
 const PaletteList = (props) => {
 
-    const { palettes } = props;
+    const { palettes, classes } = props;
 
     return (
-        <div>
-            
-            <h1>React Colors</h1>
-            { palettes.map(palette => (
-                <p>
-                    <MiniPalette {...palette } />
-                    {/* <Link to={`/palette/${palette.id}`}>{ palette.paletteName }</Link> */}
-                </p>
-            ))}
+        <div className={ classes.root }>
+            <div className={ classes.container }>
+                <nav className={ classes.nav }>
+                    <h1>React Colors</h1>
+                </nav>
+                <div className={ classes.palettes }>
+                    { palettes.map(palette => (
+                            <p>
+                                <MiniPalette {...palette } />
+                            </p>
+                    ))}
+                </div>
+            </div>
         </div>
     )
 };
 
-export default PaletteList;
+export default withStyles(styles)(PaletteList);
