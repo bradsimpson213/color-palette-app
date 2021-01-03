5d163c86f234bec1a6b4312b22abf2ed96e26320
bradsimpson213 <bsimpson@appacademy.io>
2021-01-02 22:59:09 -0500

    minipelette syles

--- src/MiniPalette.js
+++ src/MiniPalette.js
@@ -4,6 +4,7 @@ import { withStyles } from "@material-ui/styles";
 const styles = {
     root: {
        backgroundColor: "white",
+       border: "1px solid black",
        borderRadius: "5px",
        padding: "0.5rem",
        position: "relative",
@@ -16,10 +17,18 @@ const styles = {
         backgroundColor: "gray"
     },
     title: {
-        display: "flex"
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        margin: "0",
+        color: "black",
+        paddingTop: "0.5rem",
+        fontSize: "1rem",
+        position: "relative"
     },
     emoji: {
-
+        marginLeft: "0.5rem",
+        fontSize: "1.5rem"
     }
 }
 
