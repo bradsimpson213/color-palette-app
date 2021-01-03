814bb28bc894b34e7bfa27adcd78cc4f23aa7270
bradsimpson213 <bsimpson@appacademy.io>
2021-01-02 23:33:02 -0500

    complete mini palette styling

--- src/MiniPalette.js
+++ src/MiniPalette.js
@@ -14,7 +14,11 @@ const styles = {
        }
     },
     colors: {
-        backgroundColor: "gray"
+        backgroundColor: "#dae1e4",
+        height: "150px",
+        width: "100%",
+        borderRadius: "5px",
+        overflow: "hidden"
     },
     title: {
         display: "flex",
@@ -29,15 +33,29 @@ const styles = {
     emoji: {
         marginLeft: "0.5rem",
         fontSize: "1.5rem"
+    },
+    miniColor: {
+        height: "25%",
+        width: "20%",
+        display: "inline-block",
+        margin: "0 auto",
+        position: "relative",
+        marginBottom: "-3.5px"
     }
-}
+};
 
 const MiniPalette = (props) => {
-    const { classes, paletteName, emoji } = props;
+    const { classes, paletteName, emoji, colors } = props;
     return (
         <div className={ classes.root }>
             <div className={ classes.colors }>
-
+                { colors.map( color =>(
+                    <div 
+                        className={ classes.miniColor } 
+                        style={{ backgroundColor: color.color }}
+                        key={ color.name }
+                    />
+                ))}
             </div>
             <h5 className={ classes.title }>
                 { paletteName } 
