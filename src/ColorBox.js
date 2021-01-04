3ee1c714365d3e93492592de0d1311678e19ff0a
bradsimpson213 <bsimpson@appacademy.io>
2021-01-03 23:28:42 -0500

    added dynamic text color based on palette color

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -2,11 +2,14 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { CopyToClipboard } from "react-copy-to-clipboard";
 import useToggleState from "./hooks/useToggleState";
+import chroma from "chroma-js";
 import "./ColorBox.css"
 
 const ColorBox = (props) => {
     const { name, background, paletteId, id, showLink } = props;
     const [ copy, toggleCopy ] = useToggleState(false)
+    const isDarkColor = chroma(background).luminance() <= 0.08;
+    const isLightColor = chroma(background).luminance() >= 0.6
 
     const changeCopyState = () => {
         toggleCopy(); 
@@ -23,17 +26,17 @@ const ColorBox = (props) => {
                 />
                 <div className={ `copy-message ${copy && "show"}` }>
                     <h1>copied!</h1>
-                    <p>{ background }</p>
+                    <p className={ isLightColor && "dark-text" }>{ background }</p>
                 </div>
                 <div className="copy-container">
                     <div className="box-content">
-                        <span>{ name }</span>
+                        <span className={ isDarkColor && "light-text" }>{ name }</span>
                     </div>
-                    <button className="copy-button">Copy</button>
+                    <button className={ `copy-button ${ isLightColor && "dark-text" }`}>Copy</button>
                 </div>
                 { showLink && (
                 <Link to={ `/palette/${paletteId}/${id}` } onClick={ (e) => e.stopPropagation() }>
-                    <span className="see-more">More</span>    
+                    <span className={ `see-more ${ isLightColor && "dark-text" }` }>More</span>    
                 </Link>
                 )}
             </div>
