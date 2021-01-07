9467f38776d891527fb5329270d756340ebf8e1b
bradsimpson213 <bsimpson@appacademy.io>
2021-01-06 23:34:48 -0500

    finished style refactor

--- src/NavBar.js
+++ src/NavBar.js
@@ -9,57 +9,8 @@ import Snackbar from "@material-ui/core/Snackbar";
 import IconButton from "@material-ui/core/IconButton";
 import CloseIcon from "@material-ui/icons/Close";
 import "rc-slider/assets/index.css";
-// import "./NavBar.css";
+import styles from "./styles/NavbarStyles";
 
-
-const styles= {
-    Navbar: {
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'flex-start',
-        height: '6vh'
-    },
-    logo: {
-        marginRight: '15px',
-        padding: '0 13px',
-        fontSize: '22px',
-        backgroundColor: '#eceff1',
-        fontFamily: 'Roboto',
-        textDecoration: 'none',
-        height: '100%',
-        display: 'flex',
-        alignItems: 'center',
-        "& a": {
-            textDecoration: 'none',
-            color: 'black'
-        }
-    },
-    slider: {
-        width: '340px',
-        margin: '0 10px',
-        display: 'inline-block',
-        "& .rc-slider-rail": {
-            height: '8px'
-        },
-        "& .rc-slider-track": {
-            backgroundColor: 'transparent'
-        },
-        " & .rc-slider-handle, .rc-slider-handle:active, .rc-slider-handle:focus, .rc-slider-handle:hover": {
-            backgroundColor: 'green',
-            outline: 'none',
-            border: '2px solid black',
-            boxShadow: 'none',
-            width: '13px',
-            height: '13px',
-            marginLeft: '-7px',
-            marginTop: '-3px'
-        }
-    },
-    selectContainer: {
-        marginLeft: 'auto',
-        marginRight: '1rem'
-    }
-};
 
 const Navbar = (props) => {
     const { level, setLevel, multiColor, classes } = props;
