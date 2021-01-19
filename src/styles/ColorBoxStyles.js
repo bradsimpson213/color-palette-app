b4146029ae3963597bc5912529126c1fd2d877f2
bradsimpson213 <bsimpson@appacademy.io>
2021-01-18 22:01:26 -0500

    working on color box responsiveness

--- src/styles/ColorBoxStyles.js
+++ src/styles/ColorBoxStyles.js
@@ -18,12 +18,12 @@ const styles = {
         [sizes.down('lg')]: {
             width: "25%",
             height: props =>
-                props.showingFullPalette ? "20%" : "50%",
+                props.showingFullPalette ? "20%" : "33.3333%",
         },
         [sizes.down('md')]: {
             width: "50%",
             height: props =>
-                props.showingFullPalette ? "10%" : "50%",
+                props.showingFullPalette ? "10%" : "20%",
         },
         [sizes.down('xs')]: {
             width: "100%",
@@ -128,7 +128,10 @@ const styles = {
             textTransform: "uppercase",
             background: "rgba(255, 255, 255, 0.2)",
             padding: "1rem",
-            marginBottom: "0"
+            marginBottom: "0",
+            [sizes.down("xs")]: {
+                fontSize: "6rem"
+            }
         },
         "& p": {
             fontSize: "2rem",
--- src/styles/PaletteStyles.js
+++ src/styles/PaletteStyles.js
@@ -1,3 +1,6 @@
+
+import sizes from "./MediaSizes";
+
 const styles = {
     Palette: {
         height: "100vh",
@@ -35,6 +38,18 @@ const styles = {
             textTransform: "uppercase",
             textDecoration: "none",
             border: "none"
+        },
+        [sizes.down("lg")]: {
+            width: "25%",
+            height: "33.3333%"
+        },
+        [sizes.down("md")]: {
+            width: "50%",
+            height: "20%"
+        },
+        [sizes.down("xs")]: {
+            width: "100%",
+            height: "10%"
         }
     }
     
