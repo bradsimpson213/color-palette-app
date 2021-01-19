bb7e6b1a9da5aef4c33752b2c14211f40233facd
bradsimpson213 <bsimpson@appacademy.io>
2021-01-18 22:38:40 -0500

    mini palette responsiveness

--- src/styles/MediaSizes.js
+++ src/styles/MediaSizes.js
@@ -7,7 +7,8 @@
             xs: "575.98px",
             sm: "767.98px",
             md: "991.98px",
-            lg: "1190.98px"
+            lg: "1190.98px",
+            xl: "1600px"
         }
         return (`@media (max-width: ${sizes[size]})`)
     }
--- src/styles/PaletteListStyles.js
+++ src/styles/PaletteListStyles.js
@@ -1,3 +1,5 @@
+import sizes from "./MediaSizes";
+
 const styles = {
     root: {
         backgroundColor: "blue",
@@ -12,6 +14,9 @@ const styles = {
         alignItems: "flex-start",
         flexDirection: "column",
         flexWrap: "wrap",
+        [sizes.down("xl")]:{
+            width: '80%'
+        }      
     }, 
     nav: {
         display: "flex",
@@ -28,8 +33,14 @@ const styles = {
         width: "100%",
         display: "grid",
         gridTemplateColumns: "repeat(3, 30%)",
-        gridGap: "5%",
-
+        gridGap: "2.5rem",
+        [sizes.down("md")]:{
+            gridTemplateColumns: "repeat(2, 50%)",
+        },
+        [sizes.down("xs")]:{
+            gridTemplateColumns: "repeat(1, 100%)",
+            gridGap: '1rem'
+        }
     }
 };
 
